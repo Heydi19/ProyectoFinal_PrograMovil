4adeb10c922b615cdd44b2780cb093e47e660fb7
Sissle Garcia <sissleletgarcia.com>
2026-08-31 22:41:24 -0600

    App.tsx ya correguido

--- AppEstudio/App.tsx
+++ AppEstudio/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View><Text>Prueba</Text></View>
+      <RootNavigator />
     </NavigationContainer>
   );
 }
