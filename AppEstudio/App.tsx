a038823a35b89eed9a803659d25a660ff950eabe
Heydi <heydialvarado15jya@gmail.com>
2026-09-24 08:37:30 -0600

    Anexo de SafeAreaProvider en el App.tsx y correccion de error en tsconfig.json

--- AppEstudio/App.tsx
+++ AppEstudio/App.tsx
@@ -5,9 +5,11 @@ import { ThemeProvider } from './src/Context/ThemeNavigator';
 import { AuthProvider } from './src/Context/AuthContext';
 import { LanguageProvider } from './src/Context/LanguageContext';
 import { AppDataProvider } from './src/Context/AppDataContext';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
+    <SafeAreaProvider>
     <LanguageProvider>
       <ThemeProvider>
         <AuthProvider>
@@ -19,5 +21,6 @@ export default function App() {
         </AuthProvider>
       </ThemeProvider>
     </LanguageProvider>
+    </SafeAreaProvider>
   );
 }
